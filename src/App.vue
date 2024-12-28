c480b88a56eb2bbfe03d8cc856bebe376a014781
Pouya Ahmadi <poa.amd@gmail.com>
2024-12-28 13:14:23 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+const name = ref("");
+</script>
+
+<template>
+  <input v-model="name" type="text" />
+  <div class="hi">HI {{ name }}</div>
+  <HelloWorld />
+</template>
+
+<style>
+.hi {
+  border: solid 1px red;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="hi">Inside Hello World</div>
+</template>
